c05d9d1ba4155adbf937b172e02026739e420ba3
Anastasiya Semizorova <semizorova2205@mail.ru>
2021-11-07 22:58:37 +0700

    Рефакторинг

--- test_functions/func.py
+++ test_functions/func.py
@@ -1,36 +1,43 @@
 """ Модуль содержит тестовые функции """
-import numpy as np
 from typing import Union
 
+import numpy as np
 
-def math_function(points: Union[np.array, list]) -> float:
+
+def test_function_1(points: Union[np.array, list]) -> float:
     """
     Минимизируемая функция
+    Реальный минимум находится в точке (0, 0) со значением функции 0
 
     :param points: координаты точек x, y
 
     :return: значение функции в точке
     """
-    return points[0] ** 2 + 4 * points[0] * points[1] + 18 * (points[1] ** 2)
+    x, y = points
+    return x ** 2 + 4 * x * y + 18 * (y ** 2)
 
 
-def test_function(points: Union[np.array, list]) -> float:
+def test_function_2(points: Union[np.array, list]) -> float:
     """
     Минимизируемая тестовая функция
+    Реальный минимум находится в точке (5, 6) со значением функции 0
 
     :param points: координаты точек x, y
 
     :return: значение функции в точке
     """
-    return 4 * (points[0] - 5) ** 2 + (points[1] - 6) ** 2
+    x, y = points
+    return 4 * (x - 5) ** 2 + (y - 6) ** 2
 
 
-def test_function_two(points: Union[np.array, list]) -> float:
+def test_function_3(points: Union[np.array, list]) -> float:
     """
     Минимизируемая тестовая функция
+    Реальный минимум находится в точке (0, 0) со значением функции 0
 
     :param points: координаты точек x, y
 
     :return: значение функции в точке
     """
-    return 2 * (points[0] ** 2) + points[0] * points[1] + points[1] ** 2
+    x, y = points
+    return 2 * (x ** 2) + x * y + y ** 2
